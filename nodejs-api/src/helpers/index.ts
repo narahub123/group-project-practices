e1b6bec161ae6690d1f1c86821cbfcbcdac55045
nara <parknara0213@gmail.com>
2024-05-14 18:56:51 +0900

    240514 helper for authentication

---
+++ nodejs-api/src/helpers/index.ts
@@ -0,0 +1,12 @@
+// encrypt the password or create a random token
+import crypto from "crypto";
+
+const SECRET = "ANTONIO-REST-API";
+
+export const random = () => crypto.randomBytes(128).toString("base64");
+export const authentication = (salt: string, password: string) => {
+  return crypto
+    .createHmac("sha256", [salt, password].join("/"))
+    .update(SECRET)
+    .digest("hex");
+};
