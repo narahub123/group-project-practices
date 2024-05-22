27945365b70bc328ddf3fe9fa1e523e4293a907e
nara <parknara0213@gmail.com>
2024-05-23 02:26:08 +0900

    240523 improvement

--- redux-for-beginner/vanilla-redux/src/index.js
+++ redux-for-beginner/vanilla-redux/src/index.js
@@ -6,14 +6,19 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const ADD = "ADD";
+
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
   console.log(count, action);
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
@@ -27,10 +32,10 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  countStore.dispatch({ type: "ADD" });
+  countStore.dispatch({ type: ADD });
 };
 const handleMinus = () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 };
 
 add.addEventListener("click", handleAdd);
