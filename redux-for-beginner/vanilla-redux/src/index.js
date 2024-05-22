d14b84b8fef2c3d7bc4bc2a9d27982a589167c8f
nara <parknara0213@gmail.com>
2024-05-23 01:48:10 +0900

    240523 actions

--- redux-for-beginner/vanilla-redux/src/index.js
+++ redux-for-beginner/vanilla-redux/src/index.js
@@ -4,16 +4,23 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-// reducer is a function that modifies the data
-// change the data
-// return value is the value of the application
-const countReducer = (state = 0) => {
-  // code for modifying the state
-  // ...
-  return state;
+const countModifier = (count = 0, action) => {
+  console.log(count, action);
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 
-// store is a place where you store data
-const countStore = createStore(countReducer);
+const countStore = createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "ADD" });
+countStore.dispatch({ type: "MINUS" });
 
 console.log(countStore.getState());
