7a641302e2e72601dfa512cdb1e702584a4f1678
nara <parknara0213@gmail.com>
2024-05-23 13:52:39 +0900

    240523 useSelector and useDispatch

--- redux-for-beginner/vanilla-redux/src/routes/Detail.js
+++ redux-for-beginner/vanilla-redux/src/routes/Detail.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteToDo } from "../store";
 
 const Detail = () => {
-  return <div>detail</div>;
+  const { id } = useParams();
+
+  const toDos = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const toDo = toDos.find((item) => item.id === parseInt(id));
+  console.log(id);
+  console.log(toDo.text);
+
+  const onClick = () => {
+    console.log(id);
+    dispatch(deleteToDo(parseInt(id)));
+    navigate("/");
+  };
+
+  return (
+    <>
+      <h1>detail</h1>
+      {toDo.text} <button onClick={onClick}>DEL</button>
+    </>
+  );
 };
 
 export default Detail;
--- redux-for-beginner/vanilla-redux/src/routes/Home.js
+++ redux-for-beginner/vanilla-redux/src/routes/Home.js
@@ -1,15 +1,29 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addToDo, deleteToDo } from "../store";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [text, setText] = useState("");
 
+  const toDos = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  console.log(toDos);
+  console.log(dispatch);
   const onChange = (e) => {
     setText(e.target.value);
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    dispatch(addToDo(text));
     setText("");
+  };
+
+  const onClick = (id) => {
+    dispatch(deleteToDo(id));
   };
   return (
     <>
@@ -18,7 +32,18 @@ const Home = () => {
         <input type="text" value={text} onChange={onChange} />
         <button>Add</button>
       </form>
-      <ul></ul>
+      <ul>
+        {toDos.map((toDo) => {
+          return (
+            <li>
+              <Link to={`/${toDo.id}`}>
+                {toDo.text}{" "}
+                <button onClick={(id) => onClick(toDo.id)}>DEL</button>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
     </>
   );
 };
