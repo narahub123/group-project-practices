361d97fe2f535e5c08a4ec90b23cb432c2743871
nara <parknara0213@gmail.com>
2024-05-23 14:35:15 +0900

    240523 createReducer

--- redux-for-beginner/vanilla-redux/src/store.js
+++ redux-for-beginner/vanilla-redux/src/store.js
@@ -1,23 +1,34 @@
 import { createStore } from "redux";
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 export const addToDo = createAction("ADD");
 export const deleteToDo = createAction("DELETE");
 
 console.log(addToDo(), deleteToDo()); // {type: , payload: {}}
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      console.log(action);
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      console.log(action);
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-};
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case addToDo.type:
+//       console.log(action);
+//       return [{ text: action.payload, id: Date.now() }, ...state];
+//     case deleteToDo.type:
+//       console.log(action);
+//       return state.filter((toDo) => toDo.id !== action.payload);
+//     default:
+//       return state;
+//   }
+// };
+
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.unshift({ text: action.payload, id: Date.now() }); // mutate state, no return
+    })
+    .addCase(
+      deleteToDo,
+      (state, action) => state.filter((toDo) => toDo.id !== action.payload) // return new state,  immutable
+    );
+});
 
 const store = createStore(reducer);
 
