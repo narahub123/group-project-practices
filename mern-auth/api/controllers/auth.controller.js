3fd11e20303ee99f2cc502153e65652b4bab6021
nara <parknara0213@gmail.com>
2024-06-18 01:13:19 +0900

    240618 create sign in API route

--- mern-auth/api/controllers/auth.controller.js
+++ mern-auth/api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -16,5 +17,34 @@ export const signup = async (req, res, next) => {
   } catch (error) {
     next(error);
     // next(errorHandler(300, "Something went wrong"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    // validate email
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    // validate password
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "wrong credentials"));
+
+    // create token
+    const token = jwt.sign({ id: validPassword._id }, process.env.JWT_SECRET);
+
+    // destructure the value to remove password from the result
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    const expiryDate = new Date(Date.now() + 360000); // 1hour
+
+    // put token inside cookie of client side
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
