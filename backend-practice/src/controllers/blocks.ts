10305e74437d5b0a3aa5b57f13eaac442019ac1d
nara <parknara0213@gmail.com>
2024-07-17 15:24:21 +0900

    240717 blocks search

--- backend-practice/src/controllers/blocks.ts
+++ backend-practice/src/controllers/blocks.ts
@@ -13,7 +13,7 @@ export const getBlocksForAdmin = async (
   res: express.Response
 ) => {
   const { userId, role } = req.user;
-  const { sortKey, sortValue, search, keyword } = req.query;
+  let { sortKey, sortValue, search, keyword } = req.query;
 
   const key = sortKey ? sortKey.toString() : "blockDate";
   const value = sortValue.toString();
@@ -21,38 +21,26 @@ export const getBlocksForAdmin = async (
   let result;
 
   console.log(sortKey, sortValue);
+  keyword = keyword.toString() || "userNickname";
+  search = search || "";
+
+  console.log(search, keyword);
 
   // 관리자 여부 확인
   if (verfiyRole(role)) {
     // 관리자인 경우
     // result = await fetchAllBlocks()
-    //   .populate({
-    //     path: "userId",
-    //     select: "nickname",
-    //     // options: { sort: { key: value === "desc" ? -1 : 1 } },
-    //   })
-    //   .populate({
-    //     path: "blockedId",
-    //     select: "nickname",
-    //     // options: { sort: { key: value === "desc" ? -1 : 1 } },
-    //   });
   } else {
     // 사용자인 경우
-    // 유저 아이디를 추가해서 해당 아이디를 가진 목록만 반환
-    // result = fetchAllBlocks(userId)
-    //   .populate({
-    //     path: "userId",
-    //     select: "nickname",
-    //   })
-    //   .populate({
-    //     path: "blockedId",
-    //     select: "nickname",
-    //   });
     try {
       // aggregate
       result = await Block.aggregate([
         // Block 컬렉션에서 주어진 userId와 일치하는 document를 찾음
-        { $match: { userId: new mongoose.Types.ObjectId(userId) } },
+        {
+          $match: {
+            userId: new mongoose.Types.ObjectId(userId),
+          },
+        },
 
         // users 컬렉션과 조인하여 userId 필드를 기준으로 currentUser 필드에
         // 매칭된 닉네임 추가
@@ -96,11 +84,33 @@ export const getBlocksForAdmin = async (
       console.log(error);
     }
   }
+  console.log("result", result);
+  console.log("keyword", keyword);
+  console.log("search", search);
 
-  console.log("result1", result);
-  console.log("key", key);
+  // 검색 
+  let blocks =
+    keyword !== "blockDate"
+      ? result.filter((block) => {
+          return block[keyword][0].includes(search);
+        })
+      : result.filter((block) => {
+          const blockDate = new Date(block[keyword]);
 
-  let blocks = result;
+          const year = blockDate.getFullYear();
+          const month = (blockDate.getMonth() + 1).toString().padStart(2, "0");
+          const date = blockDate.getDate();
+          const blockString = `${year}-${month}-${date}`;
+
+          const comparison = search
+            ? new Date(blockString).getTime() ===
+              new Date(search.toString()).getTime()
+            : true;
+
+          return comparison;
+        });
+
+  console.log("result", blocks);
 
   return res.status(200).json(blocks);
 };
